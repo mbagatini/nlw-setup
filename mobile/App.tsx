675dc7ce6664bdbf030a7761a6bc6c0db166df11
Morgana <morganabagatini@gmail.com>
2023-01-17 17:09:48 -0300

    Status bar translucent

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
@@ -19,7 +18,7 @@ export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
+			<StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
 		</View>
 	);
 }

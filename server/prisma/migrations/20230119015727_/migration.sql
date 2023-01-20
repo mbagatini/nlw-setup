9b6ea5afb84784287cc77928dced436b6574f3d9
Morgana <morganabagatini@gmail.com>
2023-01-20 00:23:04 -0300

    Removing field fom dayHabit

---
+++ server/prisma/migrations/20230119015727_/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `completed_at` on the `day_habit` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_day_habit" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL,
+    CONSTRAINT "day_habit_day_id_fkey" FOREIGN KEY ("day_id") REFERENCES "day" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "day_habit_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habit" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_day_habit" ("day_id", "habit_id", "id") SELECT "day_id", "habit_id", "id" FROM "day_habit";
+DROP TABLE "day_habit";
+ALTER TABLE "new_day_habit" RENAME TO "day_habit";
+CREATE UNIQUE INDEX "day_habit_day_id_habit_id_key" ON "day_habit"("day_id", "habit_id");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;

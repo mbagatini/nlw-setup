0f2bf2bf4c34aeb91ca55a5292df3cbbc195fd78
Morgana <morganabagatini@gmail.com>
2023-01-20 00:23:43 -0300

    List habits

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,8 @@ import { habitsRoutes } from './routes/habits';
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
+app.use(express.json());
 app.use('/habits', habitsRoutes);
 
 app.listen(3333, () => {
--- server/src/routes/habits.ts
+++ server/src/routes/habits.ts
@@ -5,7 +5,14 @@ import { prisma } from '../services/prisma';
 export const habitsRoutes = Router();
 
 habitsRoutes.get('/', async (request, response) => {
-	const habits = await prisma.habit.findMany();
+	const habits = await prisma.habit.findMany({
+		include: {
+			weekdays: { select: { weekday: true } }
+		}
+	});
+
+	response.json(habits);
+});
 
 	return response.json(habits);
 });

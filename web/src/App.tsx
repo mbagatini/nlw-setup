8baea9ed35a113d1ab9f33d53f57fb9ecb592558
Morgana <morganabagatini@gmail.com>
2023-01-21 23:23:54 -0300

    COnfiguring tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello nlw</h1>
-    </div>
-  )
-}
+import './styles/global.css';
 
-export default App
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
+
+export function App() {
+	return (
+		<div className='w-screen h-screen flex items-center justify-center'>
+			<div className='w-full max-w-5xl px-6 flex flex-col gap-16 text-white'>
+				<Header />
+
+				<SummaryTable />
+			</div>
+		</div>
+	)
+}

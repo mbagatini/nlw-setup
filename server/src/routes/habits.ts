a2e04f2fee4c6c4354f161d5f72779b5a06e23ec
Morgana <morganabagatini@gmail.com>
2023-01-23 17:54:19 -0300

    Toggle habit competion route

--- server/src/routes/habits.ts
+++ server/src/routes/habits.ts
@@ -86,3 +86,65 @@ habitsRoutes.post('/', async (request, response, next) => {
 		next(error);
 	}
 });
+
+habitsRoutes.patch('/:id/toggle', async (req, res, next) => {
+	const routeParams = z.object({
+		id: z.string().uuid(),
+	});
+
+	try {
+		const { id } = routeParams.parse(req.params);
+
+		const habit = await prisma.habit.findUnique({
+			where: { id }
+		});
+
+		if (!habit) {
+			return res.status(404).send('Habit not found');
+		}
+
+		// Check day is created
+		const today = dayjs().startOf('day').toDate();
+
+		let day = await prisma.day.findUnique({
+			where: { date: today }
+		});
+
+		if (!day) {
+			day = await prisma.day.create({
+				data: {
+					date: today
+				}
+			});
+		}
+
+		// Toggle the habit completion
+		const dayHabitExists = await prisma.dayHabit.findUnique({
+			where: {
+				day_id_habit_id: {
+					day_id: day.id,
+					habit_id: id
+				}
+			}
+		});
+
+		if (dayHabitExists) {
+			await prisma.dayHabit.delete({
+				where: {
+					id: dayHabitExists.id
+				}
+			})
+		} else {
+			await prisma.dayHabit.create({
+				data: {
+					habit_id: id,
+					day_id: day.id
+				}
+			});
+		}
+
+		res.status(201).send();
+	} catch (error) {
+		next(error);
+	}
+})

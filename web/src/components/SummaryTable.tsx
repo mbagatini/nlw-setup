93d08943a55b4c907b0b4bcd2673bfe73c273311
Morgana <morganabagatini@gmail.com>
2023-01-25 08:55:17 -0300

    Summary table: get data from api

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,14 +1,26 @@
+import dayjs from "dayjs";
 import { generateDatesFromYearBeginning } from "../utils/date-functions";
 import { HabitDay } from "./HabitDay";
 
-export function SummaryTable() {
-	const weekdays: string[] = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
-	const dates = generateDatesFromYearBeginning();
+const weekdays: string[] = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const dates = generateDatesFromYearBeginning();
 
-	// Calculates the empty boxes in summary
-	const minimumSummaryDatesAmount = 18 * 7;
-	const datesToFillAmount = minimumSummaryDatesAmount - dates.length;
+// Calculates the empty boxes in summary
+const minimumSummaryDatesAmount = 18 * 7;
+const datesToFillAmount = minimumSummaryDatesAmount - dates.length;
 
+type SummaryHabits = {
+	id: string,
+	date: string,
+	amount: number,
+	completed: number
+}
+
+interface SummaryTableProps {
+	data: SummaryHabits[]
+}
+
+export function SummaryTable({ data }: SummaryTableProps) {
 	return (
 		<div className="w-full flex gap-4">
 			<div className="grid grid-rows-7 gap-3">
@@ -22,9 +34,20 @@ export function SummaryTable() {
 			</div>
 
 			<div className="grid grid-rows-7 grid-flow-col gap-3">
-				{dates.map(date => (
-					<HabitDay key={date.toString()} data={{ amount: 15, completed: Math.random() * 15 }} />
-				))}
+				{dates.map(date => {
+					const dayInSummary = data.find(day => {
+						return dayjs(date).isSame(day.date, 'day');
+					})
+
+					return <HabitDay
+						key={date.toString()}
+						data={{
+							date: date.toString(),
+							amount: dayInSummary?.amount ?? 0,
+							completed: dayInSummary?.completed ?? 0
+						}}
+					/>
+				})}
 
 				{datesToFillAmount > 0 && Array.from({ length: datesToFillAmount }).map((value, i) =>
 					<div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />

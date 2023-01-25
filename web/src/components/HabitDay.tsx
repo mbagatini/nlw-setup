c4998e843009972a7450ca6d3a0007369fda5cc2
Morgana <morganabagatini@gmail.com>
2023-01-25 09:10:57 -0300

    Habits of the day: data from api

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,27 +1,57 @@
 import * as Popover from '@radix-ui/react-popover';
 import * as Progress from '@radix-ui/react-progress';
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Checkbox } from './Checkbox';
+import { api } from '../lib/api';
+import dayjs from 'dayjs';
 
-interface HabitProps {
+type SummaryHabits = {
+	date: string;
 	amount: number;
 	completed: number;
 }
 
-interface HabitDayProps {
-	data: HabitProps;
+type Habit = {
+	id: string;
+	title: string;
+	created_at: string;
 }
 
-export function HabitDay({ data }: HabitDayProps) {
-	const completedPercentage = Math.round((data.completed / data.amount) * 100);
-	const [progress, setProgress] = useState(completedPercentage);
+type DayHabits = {
+	target: Habit[]
+	completed: string[]
+}
 
-	const checkboxClass = "font-semibold text-xl leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400";
+interface HabitDayProps {
+	data: SummaryHabits;
+}
+
+export function HabitDay({ data: day }: HabitDayProps) {
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [dayHabits, setDayHabits] = useState<DayHabits>({} as DayHabits);
+
+	const completedPercentage = day.amount == 0 ? 0 : Math.round((day.completed / day.amount) * 100);
+
+	const formattedDate = dayjs(day.date).format('DD/MM');
+	const weekdayDescription = dayjs(day.date).format('dddd');
+
+	useEffect(() => {
+		if (modalIsOpen) {
+			api.get('/habits/day', {
+				params: {
+					date: day.date
+				}
+			}).then(response => {
+				setDayHabits(response.data);
+			})
+		}
+
+	}, [modalIsOpen])
 
 	return (
-		<Popover.Root>
+		<Popover.Root onOpenChange={status => setModalIsOpen(status)}>
 			<Popover.Trigger
 				className={clsx("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg", {
 					'bg-violet-900 border-violet-700': completedPercentage >= 1 && completedPercentage < 20,
@@ -33,21 +63,30 @@ export function HabitDay({ data }: HabitDayProps) {
 			/>
 			<Popover.Portal>
 				<Popover.Content sideOffset={5} className="min-w-[320px] bg-zinc-900 rounded-2xl p-6 text-white flex flex-col">
-					<span className='text-zinc-400 font-semibold'>terça-feira</span>
-					<span className='font-extrabold text-3xl leading-tight mt-2 mb-4'>03/01</span>
+					<span className='text-zinc-400 font-semibold'>{weekdayDescription}</span>
+					<span className='font-extrabold text-3xl leading-tight mt-2 mb-4'>{formattedDate}</span>
 
 					<Progress.Root className="h-3 bg-zinc-700 rounded-xl mb-6">
 						<Progress.Indicator className="h-3 w-3/4 bg-violet-600 rounded-xl"
 							aria-label='Progresso dos hábitos completados no dia'
-							style={{ width: `${progress}%` }}
+							style={{ width: `${completedPercentage}%` }}
 						/>
 					</Progress.Root>
 
-					<div className='flex flex-col gap-3'>
-						<Checkbox title="minha atividade" titleStyle={checkboxClass} />
-						<Checkbox title="minha atividade" titleStyle={checkboxClass} />
-						<Checkbox title="minha atividade" titleStyle={checkboxClass} />
-					</div>
+					{dayHabits.target && (
+						<div className='flex flex-col gap-3'>
+							{dayHabits.target.map(habit => {
+								const habitCompleted = dayHabits.completed && dayHabits.completed.find(id => id === habit.id);
+
+								return <Checkbox
+									key={habit.id}
+									title={habit.title}
+									checked={!!habitCompleted}
+									titleStyle="font-semibold text-xl leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400"
+								/>
+							})}
+						</div>
+					)}
 
 					<Popover.Arrow height={8} width={16} className="fill-zinc-900" />
 				</Popover.Content>

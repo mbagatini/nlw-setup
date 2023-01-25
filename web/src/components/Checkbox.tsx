10a4ab685f5d74d842d24f64769c63311d49a433
Morgana <morganabagatini@gmail.com>
2023-01-24 22:28:07 -0300

    Handle checkbox state change

--- web/src/components/Checkbox.tsx
+++ web/src/components/Checkbox.tsx
@@ -1,20 +1,21 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
-interface CheckboxProps extends CheckboxPrimitive.CheckboxIndicatorProps {
+interface Teste extends CheckboxPrimitive.CheckboxProps {
 	title: string;
+	titleStyle?: string;
 }
 
-export function Checkbox({ title, ...rest }: CheckboxProps) {
+export function Checkbox({ title, titleStyle, ...rest }: Teste) {
 	return (
-		<CheckboxPrimitive.Root className="flex gap-3 items-center group">
+		<CheckboxPrimitive.Root className="flex gap-3 items-center group" {...rest}>
 			<div className="w-8 h-8 border-2 border-zinc-800 rounded-lg flex items-center group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-none text-white"
 			>
 				<CheckboxPrimitive.Indicator className="p-1.5">
 					<Check size={20} />
 				</CheckboxPrimitive.Indicator>
 			</div>
-			<span className={`text-white ${rest.className}`}>{title}</span>
+			<span className={`text-white ${titleStyle}`}>{title}</span>
 		</CheckboxPrimitive.Root >
 	)
 }
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,17 +1,31 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { Check, X } from 'phosphor-react';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 import { Checkbox } from './Checkbox';
 
 export function NewHabitForm() {
 	const [title, setTitle] = useState("");
+	const [weekdays, setWeekdays] = useState<string[]>([]);
+
+	function handleWeekdaysChange(checked: boolean, value: string) {
+		if (checked === true) {
+			setWeekdays([...weekdays, value]);
+		} else {
+			const updatedWeekdays = weekdays.filter(w => w !== value);
+			setWeekdays(updatedWeekdays);
+		}
+	}
+
+	function handleHabitSubmition(e: FormEvent) {
+		e.preventDefault();
+	}
 
 	return (
 		<>
 			<Dialog.Title className="text-white font-extrabold text-3xl leading-tight pb-6">Criar hábito</Dialog.Title>
 
-			<form className='text-white w-full'>
+			<form onSubmit={handleHabitSubmition} className='text-white w-full'>
 				<fieldset className='flex flex-col gap-3 mb-4'>
 					<label className="font-semibold" htmlFor="title">
 						Qual seu comprometimento?
@@ -29,24 +43,22 @@ export function NewHabitForm() {
 						Qual a recorrência?
 					</label>
 
-					<Checkbox title="Segunda-feira" />
-					<Checkbox title="Terça-feira" />
-					<Checkbox title="Quarta-feira" />
-					<Checkbox title="Quinta-feira" />
-					<Checkbox title="Sexta-feira" />
-					<Checkbox title="Sábado" />
-					<Checkbox title="Domingo" />
+					<Checkbox title="Segunda-feira" onCheckedChange={isChecked => handleWeekdaysChange(isChecked === true, "1")} />
+					<Checkbox title="Terça-feira" onCheckedChange={isChecked => handleWeekdaysChange(isChecked === true, "2")} />
+					<Checkbox title="Quarta-feira" onCheckedChange={isChecked => handleWeekdaysChange(isChecked === true, "3")} />
+					<Checkbox title="Quinta-feira" onCheckedChange={isChecked => handleWeekdaysChange(isChecked === true, "4")} />
+					<Checkbox title="Sexta-feira" onCheckedChange={isChecked => handleWeekdaysChange(isChecked === true, "5")} />
+					<Checkbox title="Sábado" onCheckedChange={isChecked => handleWeekdaysChange(isChecked === true, "6")} />
+					<Checkbox title="Domingo" onCheckedChange={isChecked => handleWeekdaysChange(isChecked === true, "0")} />
 				</fieldset>
 
-				<Dialog.Close asChild>
-					<button className="w-full p-4 mt-6 rounded-lg flex items-center justify-center gap-3 bg-green-600 hover:bg-green-500 font-semibold ">
-						<Check size={24} />
-						Confirmar
-					</button>
-				</Dialog.Close>
+				<button type='submit' className="w-full p-4 mt-6 rounded-lg flex items-center justify-center gap-3 bg-green-600 hover:bg-green-500 font-semibold ">
+					<Check size={24} />
+					Confirmar
+				</button>
 			</form>
 
-			<Dialog.Close asChild className='absolute top-6 right-6 text-zinc-400 hover:text-zinc-200'>
+			<Dialog.Close className='absolute top-6 right-6 text-zinc-400 hover:text-zinc-200'>
 				<X size={24} aria-label="Close" />
 			</Dialog.Close>
 		</>

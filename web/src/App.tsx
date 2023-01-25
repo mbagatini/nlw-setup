2d5a13e47f2d097a533dbeea4ba1e7d6b6a0b436
Morgana <morganabagatini@gmail.com>
2023-01-25 08:55:09 -0300

    Summary table: get data from api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,33 @@
-import './styles/global.css';
+import { useEffect, useState } from 'react';
 
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
+import { api } from './lib/api';
+
+import './lib/dayjs.ts';
+import './styles/global.css';
+
+type SummaryHabits = {
+	id: string,
+	date: string,
+	amount: number,
+	completed: number
+}
 
 export function App() {
+	const [summary, setSummary] = useState<SummaryHabits[]>([]);
+
+	useEffect(() => {
+		api.get('/habits/summary').then(response => {
+			setSummary(response.data);
+		});
+	}, []);
+
 	return (
 		<div className='w-screen h-screen flex items-center justify-center'>
 			<div className='w-full max-w-5xl px-6 flex flex-col gap-16 text-white'>
 				<Header />
-				<SummaryTable />
+				<SummaryTable data={summary} />
 			</div>
 		</div>
 	)

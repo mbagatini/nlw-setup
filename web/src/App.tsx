0b8bfb9f46ca37d26301f388b7d53a1992a10635
Morgana <morganabagatini@gmail.com>
2023-02-01 08:47:55 -0300

    Push notification using service worker

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,23 @@ import { SummaryTable } from './components/SummaryTable';
 import './lib/dayjs.ts';
 import './styles/global.css';
 
+// Register service worker
+if ("serviceWorker" in navigator) {
+	try {
+		navigator.serviceWorker.register("/service-worker.js").then(registration => {
+			if (registration.installing) {
+				console.log("Service worker installing");
+			} else if (registration.waiting) {
+				console.log("Service worker installed");
+			} else if (registration.active) {
+				console.log("Service worker active");
+			}
+		})
+	} catch (error) {
+		console.error(`Registration failed with ${error}`);
+	}
+}
+
 export function App() {
 	return (
 		<div className='w-screen h-screen flex items-center justify-center'>

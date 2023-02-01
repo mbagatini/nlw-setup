bb0df54178364e058b79991e282f9398f31c7d38
Morgana <morganabagatini@gmail.com>
2023-02-01 15:06:33 -0300

    Implementation of subscription and testing notification

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
+import { api } from './lib/api';
 
 import './lib/dayjs.ts';
 import './styles/global.css';
@@ -8,13 +9,30 @@ import './styles/global.css';
 // Register service worker
 if ("serviceWorker" in navigator) {
 	try {
-		navigator.serviceWorker.register("/service-worker.js").then(registration => {
+		navigator.serviceWorker.register("/service-worker.js").then(async registration => {
 			if (registration.installing) {
 				console.log("Service worker installing");
 			} else if (registration.waiting) {
 				console.log("Service worker installed");
 			} else if (registration.active) {
 				console.log("Service worker active");
+
+				let subscription = await registration.pushManager.getSubscription();
+
+				const { data } = await api.get('/push/public_key');
+
+				subscription = await registration.pushManager.subscribe({
+					userVisibleOnly: true,
+					applicationServerKey: data.public_key
+				});
+
+				await api.post('/push/register', {
+					subscription
+				});
+
+				await api.post('/push/send', {
+					subscription
+				});
 			}
 		})
 	} catch (error) {
